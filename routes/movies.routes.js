ef8fb3ecd35e059fff2c91aa88ef842c6bb7d586
Marco Zampini <marco@bloom-it.io>
2022-03-22 17:03:24 +0100

    Iteration 6 | Adding New Movies

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,5 +1,34 @@
 const router = require('express').Router()
 
-// all your routes here
+const Movie = require('../models/Movie.model')
+const Celebrity = require('../models/Celebrity.model')
+
+router.get('/create', async (req, res, next) => {
+  try {
+    const celebrities = await Celebrity.find()
+    res.render('movies/new-movie', { celebrities })
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/create', async (req, res, next) => {
+  try {
+    await Movie.create(req.body)
+    res.redirect(`${req.baseUrl}`)
+  } catch (error) {
+    console.log(error)
+    res.redirect(`${req.baseUrl}/create`)
+  }
+})
+
+router.get('/', async (req, res, next) => {
+  try {
+    const movies = await Movie.find()
+    res.render('movies/movies', { movies })
+  } catch (error) {
+    next(error)
+  }
+})
 
 module.exports = router
