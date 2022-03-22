1097f2f5b4c1ffc0eee26025a71ccc0f422b8011
Marco Zampini <marco@bloom-it.io>
2022-03-22 14:30:55 +0100

    Iteration 3 | Adding New Celebrities

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,19 @@
 const router = require('express').Router()
 
-// all your routes here
+const Celebrity = require('../models/Celebrity.model')
+
+router.get('/create', (req, res, next) => {
+  res.render('celebrities/new-celebrity')
+})
+
+router.post('/create', async (req, res, next) => {
+  try {
+    await Celebrity.create(req.body)
+    res.redirect('/')
+  } catch (error) {
+    console.log(error)
+    res.redirect('/create')
+  }
+})
 
 module.exports = router
